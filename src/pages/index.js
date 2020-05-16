be84cd1f92982e2dcad729a86045ece37f031397
Luis Carlin <luiscarlin@gmail.com>
2020-05-16 16:49:55 -0400

    more cleanup; render welcome

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,10 @@
-import { Link } from "gatsby"
 import React from "react"
-import Image from "../components/image"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
-
+import styles from "./index.module.scss"
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
     <h1>I&rsquo;m Luis.</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1 className={styles.accentTitle}>Web & Mobile developer</h1>
   </Layout>
 )
 
