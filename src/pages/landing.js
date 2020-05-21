14e7d52925d78a3e9e6094a01689aee3bd495649
Luis Carlin <luiscarlin@gmail.com>
2020-05-21 18:09:07 -0400

    highlight on social media bar

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,52 +1,13 @@
 import React from "react"
-import {
-  FaEnvelopeSquare,
-  FaGithubSquare,
-  FaLinkedin,
-  FaTwitterSquare,
-} from "react-icons/fa"
-
-const SocialMediaBar = () => {
-  const socialMedia = [
-    {
-      icon: <FaGithubSquare />,
-      url: "https://github.com/luiscarlin",
-    },
-    {
-      icon: <FaLinkedin />,
-      url: "https://www.linkedin.com/in/luis-carlin-48512649/",
-    },
-    {
-      icon: <FaTwitterSquare />,
-      url: "https://twitter.com/lgcarlin",
-    },
-    {
-      icon: <FaEnvelopeSquare />,
-      url: "mailto:luiscarlin@gmail.com",
-    },
-  ]
-
-  return (
-    <div>
-      {socialMedia.map((site, index) => (
-        <a
-          key={index}
-          href={site.url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {site.icon}
-        </a>
-      ))}
-    </div>
-  )
-}
+import SocialMediaBar from "../components/SocialMediaBar"
 
 const Landing = () => (
   <div className="welcome">
     <h1>I&rsquo;m Luis.</h1>
-    <h1 className="welcome__accent">Web & Mobile developer.</h1>
-    {/* <SocialMediaBar /> */}
+    <h1 className="secondary">Web & Mobile developer.</h1>
+    <div className="welcome__social-media">
+      <SocialMediaBar />
+    </div>
   </div>
 )
 
