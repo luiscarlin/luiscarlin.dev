19d4f4742159d8b4168318ea09b97a2b62424de4
Luis Carlin <luiscarlin@gmail.com>
2020-05-22 17:50:05 -0400

    style social bar differently

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,15 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Header from "../Header"
 import styles from "./layout.module.scss"
 
 const Layout = ({ children, location }) => {
   return (
     <div className={styles.layout}>
+      <Helmet>
+        <title>Hey. I'm Luis</title>
+        <meta name="description" content="My personal website" />
+      </Helmet>
       <Header pathname={location.pathname} />
       <main>{children}</main>
     </div>
