815be6fb828f13b64c79dee1233b62a1ef99e127
Luis Carlin <luiscarlin@gmail.com>
2020-06-21 23:01:32 -0400

    add about me

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -4,10 +4,16 @@ import TimelineCard from "../components/TimelineCard"
 
 const content = [
   {
+    date: "March 2019",
+    title: "Got promoted to Senior Developer",
+    text: "",
+    tech: [],
+  },
+  {
     date: "September 2017",
-    title: "Started working at Pillar Technology 🎊",
+    title: "Started working at Pillar Technology",
     text:
-      "Started working at Pillar Technology, a medium sized consulting firm. In 2019, it would become part of Accenture.",
+      "Started working at Pillar Technology, a medium sized consulting firm. In March 2019, it would become part of Accenture.",
     tech: [],
   },
   {
@@ -17,19 +23,52 @@ const content = [
     tech: [],
   },
   {
-    date: "January 2012",
+    date: "January 2013",
     title: "Started working at IBM",
-    text:
-      "I was part of a four week proof of concept for a popular oil company in a team of 6 developers. The goal was to ",
-    tech: [
-      "Jenkins",
-      "Bash",
-      "UrbanCode Deploy with Patterns",
-      "Git",
-      "Gitlab",
-      "Kafka",
-      "WebSphere Liberty",
-    ],
+    text: "",
+    tech: [],
+  },
+  {
+    date: "December 2012",
+    title: "Graduated from the University of Kentucky",
+    text: "",
+    tech: [],
+  },
+  {
+    date: "May 2012",
+    title: "Intership at Lexmark",
+    text: "",
+    tech: [],
+  },
+  {
+    date: "May 2011",
+    title: "Internship Escent Technologies",
+    text: "",
+    tech: [],
+  },
+  {
+    date: "October 2010",
+    title: "Became a Naturalized American Citizen",
+    text: "",
+    tech: [],
+  },
+  {
+    date: "August 2007",
+    title: "Start College",
+    text: "",
+    tech: [],
+  },
+  {
+    date: "February 2007",
+    title: "Got Married",
+    text: "",
+    tech: [],
+  },
+  {
+    date: "July 2003",
+    title: "Goodbye Peru, Hello USA",
+    text: "",
+    tech: [],
   },
 ]
 
